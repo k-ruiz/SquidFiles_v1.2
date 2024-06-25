c8aebb83652a603f35efbba90acc410325040e38
Kyra Ruiz <kyramruiz001@gmail.com>
2024-06-25 09:39:57 -0700

    introducing a fuction to plot the stokeslet

--- MininimalMain.m
+++ MininimalMain.m
@@ -20,3 +20,15 @@ stks = getStokesletPositions(rho,geometry_type,system,U0);
 
 %% Get the flow over the space
 [Uflowx,Uflowy] = calculateFlowGrid_serial2(stks,iS,x,y,eps);
+
+%% Plot output flow field
+% This section calculates the local magnitude of the flow the create the colourised background. 
+% Then the quiver plot can be overlaid on top of this to give the directions of the flow.
+
+n=5; % Plot coarseness
+Umag = sqrt(Uflowx.^2 + Uflowy.^2); % Get flow field magnitude
+imagesc(y,x,Umag) % Plot local flow strenght as a background
+hold on
+scatter(stks(:,2),stks(:,1),2,'r') % Plot the Stokeslets
+quiver(y(1:n:end),x(1:n:end),Uflowy(1:n:end,1:n:end),Uflowx(1:n:end,1:n:end),2,'Color','w') % Plot the vector field
+axis equal
